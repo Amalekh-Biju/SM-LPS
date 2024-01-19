2cf90dce272ae4d87e1f1d0a57bcd64f6f71c9d0
Amalekh Biju <bamalekh@gmail.com>
2024-01-19 18:10:57 +0530

    Populating Content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import './header.css'
 import LOGO from '../../assets/images/Logo.png'
 import { Link } from 'react-router-dom'
-import { CiMenuKebab } from 'react-icons/ci'
+import { HiOutlineMenuAlt1 } from 'react-icons/hi'
 import { PiEyeClosedBold } from 'react-icons/pi'
 
 const showSidebar = () => {
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.display = 'flex';
+  sidebar.style.opacity = '1';
 }; 
 
 const hideSidebar = () => {
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.display = 'none';
+  sidebar.style.opacity = '0';
 }; 
 
 const Header = () => {
@@ -21,14 +23,14 @@ const Header = () => {
       <div class="landing-page">
         <header>
           <div class="container">
-            <a href="/" class="logo"><img src={LOGO} alt="logo"/></a>
+            <a href="/" class="logo"><img src={LOGO} alt="logo "/></a>
             <ul class="links">
               <li class="hide-on-mobile"><Link to="/">Home </Link></li>
               <li class="hide-on-mobile"><Link to="/about">About Us</Link></li>
               <li class="hide-on-mobile"><Link to="/testimonials">Testimonials</Link></li>
               <li class="hide-on-mobile"><Link to="/info">Info</Link></li>
               <li class="hide-on-mobile"><Link to="/admissions">Admissions</Link></li>
-              <li class="menu-btn" onClick={showSidebar}><Link to="#"><CiMenuKebab /></Link></li>
+              <li class="menu-btn" onClick={showSidebar}><Link to="#"><HiOutlineMenuAlt1 /></Link></li>
             </ul>
             <ul class="sidebar">
               <li onClick={hideSidebar}><Link to="#"><PiEyeClosedBold /> </Link></li>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -23,6 +23,13 @@ const Home = () => {
             </div >
           </div>
         </div>
+        <div className="courses">
+              <h2>What We Offer</h2>
+              <h1>Our Courses</h1>
+              <p>"St. Mary's LP School Kaliyar, a beacon of educational excellence since 1950, 
+                provides top-tier education up to standard 4. Under the guidance of the Corporate 
+                Educational Agency of Epharchy of Kothamangalam, we nurture young minds for a bright future."</p>
+        </div>
     </div>
    </div>
   )
