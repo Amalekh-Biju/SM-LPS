0d66696f840ef060c9ba6e455865622b754d1ef3
bamalekh <bamalekh@gmail.com>
2024-01-23 11:48:22 +0000

    Started some file import

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,8 +5,17 @@ import './about.css'
 import BG1 from '../../assets/BG-Profile/1.png'
 import BG2 from '../../assets/BG-Profile/2.png'
 import BG3 from '../../assets/BG-Profile/3.png'
+/* Started Pictures From Here */
+import Shibimol from '../../assets/BG-Profile/Pic/Shibimol Joseph.svg'
+import Aleena from '../../assets/BG-Profile/Pic/ALEENA SHAJU.svg'
+import Anumol from '../../assets/BG-Profile/Pic/ANUMOL MATHEW.svg'
+import Asha from '../../assets/BG-Profile/Pic/Asha jolly.svg'
+import Ashwathy from '../../assets/BG-Profile/Pic/Aswathy Bhaskar.svg'
 
-import Shibimol from '../../assets/BG-Profile/Pic/Shibimol_Joseph.png'
+import Dainy from '../../assets/BG-Profile/Pic/Dainy Saju.svg'
+import Divya from '../../assets/BG-Profile/Pic/Divya S.svg'
+
+/* Ended Pictures Import  */
 
 const About = () => {
   return (
